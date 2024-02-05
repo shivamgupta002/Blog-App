4c876bbd368dec081034cd8f2935dab36c41d873
shivamgupta002 <shivamgupta992909@gmail.com>
2024-02-05 14:08:01 +0530

    Dekstop navbar done

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,42 @@
+import styles from "./links.module.css";
+import NavLink from "./navLink/navLink";
+const Links = () => {
+  const links = [
+    {
+      title: "HomePage",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+    {
+      title: "Blog",
+      path: "/blog",
+    },
+  ];
+
+  //Temporary
+  const session = true;
+  const isAdmin = true;
+  return (
+    <div className={styles.link}>
+      {links.map((link) => (
+        <NavLink item={link} key={link.title} />
+      ))}
+      {session ? (
+        <>
+          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+          <button className={styles.logout}>Logout</button>
+        </>
+      ) : (
+        <NavLink item={{ title: "login", path: "/login" }} />
+      )}
+    </div>
+  );
+};
+export default Links;

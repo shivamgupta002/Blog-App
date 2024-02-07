3a075f6861c2274b2da70f84dfd5afbcc9dcd885
shivamgupta002 <shivamgupta992909@gmail.com>
2024-02-07 22:45:55 +0530

    footer done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,13 @@
+import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <>
-      <div>Footer </div>
+      <div className={styles.container}>
+        <div className={styles.logo}>shivam</div>
+        <div className={styles.text}>
+          shivam creative thought agency @All rights Reversed
+        </div>
+      </div>
     </>
   );
 };

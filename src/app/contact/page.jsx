1e80ba2df269ec368e46125c9e5e355fcbf8e9a6
shivamgupta002 <shivamgupta992909@gmail.com>
2024-02-07 23:45:56 +0530

    contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,24 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
 const Contact = () => {
-    return (
-      <>
-        <div>Contact Page</div>
-      </>
-    );
-  };
-  
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image src="/contact.png" alt="contactImg" fill className={styles.img} />
+        </div>
+        <div className={styles.formContainer}>
+          <form action="" className={styles.form}>
+            <input type="text" placeholder="Name and Surname" />
+            <input type="email" placeholder="Email Address" />
+            <input type="number" placeholder="Phone number (optional)" />
+            <textarea name="" id="" cols="30" rows="10" placeholder="Message" />
+            <button> Submit </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default Contact;

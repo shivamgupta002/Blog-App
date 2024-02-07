c2bced32b0c4a4019b42aa20bed88b9db7b6afe8
shivamgupta002 <shivamgupta992909@gmail.com>
2024-02-07 23:13:45 +0530

    homepage ui edit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,34 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./home.module.css";
 
 export default function Home() {
   return (
     <>
-      <div>hello world</div>
+      <div className={styles.container}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.title}>Creative Thoughts Agency</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            odio expedita labore maiores aspernatur quibusdam distinctio iure
+            velit illo sunt.
+          </p>
+          <div className={styles.buttons}>
+            <button className={styles.button}>Learn More</button>
+            <button className={styles.button}>Contact</button>
+          </div>
+          <div className={styles.brands}>
+            <Image
+              src="/brands.png"
+              alt="brands"
+              fill
+              className={styles.brandImg}
+            />
+          </div>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image src="/hero.gif" alt="test" fill className={styles.heroImg} />
+        </div>
+      </div>
     </>
   );
 }

9525b0efcb05a36094884f0f703ce0fc67ab59cf
shivamgupta002 <shivamgupta992909@gmail.com>
2024-02-07 22:35:35 +0530

    home page ui created

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
+import styles from './about.module.css';
 const About = () => {
   return (
     <>
-      <div>About Page</div>
+      <div className={styles.imgContainer}>
+        <Image src="/about.png" alt="aboutImage" fill />
+      </div>
     </>
   );
 };

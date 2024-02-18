749008a4a7a0c28424c174dc1b38a6f77ff66bfa
shivamgupta002 <shivamgupta992909@gmail.com>
2024-02-18 15:42:23 +0530

    update postcard

--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./postCard.module.css";
 
-const PostCard = () => {
+const PostCard = ({ post }) => {
   return (
     <>
       <div className={styles.container}>
@@ -13,9 +13,11 @@ const PostCard = () => {
           <span className={styles.date}>01.01.2024</span>
         </div>
         <div className={styles.bottom}>
-          <h1 className={styles.title}>Title</h1>
-          <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, voluptas.</p>
-          <Link href="/blog" className={styles.link}>Read More </Link>
+          <h1 className={styles.title}>{post.title}</h1>
+          <p className={styles.desc}>{post.body}</p>
+          <Link href={`/blog/${post.id}`} className={styles.link}>
+            Read More{" "}
+          </Link>
         </div>
       </div>
     </>

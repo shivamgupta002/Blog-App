12460df26fb01f06946b65a59e2b9077e104b965
shivamgupta002 <shivamgupta992909@gmail.com>
2024-02-18 15:41:51 +0530

    update blog page frontend using api

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,21 +1,24 @@
 import styles from "./blog.module.css";
 import PostCard from "@/components/postCard/postCard";
-const Blog = () => {
+
+const getData = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts",{next:{revalidate:3600}});
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+  return res.json();
+};
+const Blog = async () => {
+  const posts = await getData();
+
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.post}>
-          <PostCard />
-        </div>
-        <div className={styles.post}>
-          <PostCard />
-        </div>
-        <div className={styles.post}>
-          <PostCard />
-        </div>
-        <div className={styles.post}>
-          <PostCard />
-        </div>
+        {posts.map((post) => (
+          <div className={styles.post} key={post.id}>
+            <PostCard post={post} />
+          </div>
+        ))}
       </div>
     </>
   );
